c8c3480be993b8ffa1ddc35505dbf89fa233c7a1
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-26 07:17:14 +0300

    learnt about the tree structures in python

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        def traverse(node):
+            if not node:
+                return None
+            if node.get('id') == id:
+                return node
+            for child in node.get('children', []):
+                result = traverse(child)
+                if result:
+                    return result
+            return None
+
+        return traverse(self.root)
